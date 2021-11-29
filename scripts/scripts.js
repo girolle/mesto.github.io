e4aba772061e773cabba1f1eb3f17b0b201d91bc
SergeyKoloskov <isusefulin@gmail.com>
2021-11-29 11:50:28 +0300

    release

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,20 @@
-let openPopupBtn = document.querySelector(".js-profile__textEditBtn");
+let openPopupBtn = document.querySelector(".js-profile__text-edit-btn");
 let noScroll = document.querySelector(".js-root");
 let popup = document.querySelector(".js-popup");
-let closeFormBtn = document.querySelector(".js-popup__closeForm");
+let closeFormBtn = document.querySelector(".js-popup__close-form");
 let popupForm = document.querySelector(".js-popup__form");
-let popupInputName = document.querySelector(".js-popup__inputName");
-let popupInputJob = document.querySelector(".js-popup__inputJob");
+let popupInputName = document.querySelector(".js-popup__input-name");
+let popupInputJob = document.querySelector(".js-popup__input-job");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 
 function openPopup() {
   popup.classList.add("popup_active");
-  noScroll.classList.add("root_noScroll");
+  noScroll.classList.add("root_no-scroll");
 }
 function closeForm() {
   popup.classList.remove("popup_active");
-  noScroll.classList.remove("root_noScroll");
+  noScroll.classList.remove("root_no-scroll");
 }
 
 popupInputName.value = profileTitle.textContent;
